79d525459e22fe8827eed7a1d5f2e3b80be1c77d
ooknn <ggllyyx@gmail.com>
2019-08-10 23:53:58 +0800

    modify pynvim

--- nvim_install.sh
+++ nvim_install.sh
@@ -12,8 +12,9 @@ function install_prepare_software_on_centos()
 # 安装ubuntu发行版必要软件
 function install_prepare_software_on_ubuntu()
 {
-    ${InstallCommand} ctags build-essential python-dev python3-dev python-pip python3-pip
-    pip install neovim
+    ${InstallCommand} ctags build-essential python-dev python3-dev && \
+    ${InstallCommand} python-pip python3-pip && \
+    pip install neovim && \
     pip3 install neovim
 }
 
