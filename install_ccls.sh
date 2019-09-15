f63cce4232330b8b8044cb209d64ca505106cbf4
ggyyll <im.ggyyll@gmail.com>
2019-09-15 23:37:55 +0800

    Update depth

--- install_ccls.sh
+++ install_ccls.sh
@@ -26,8 +26,8 @@ function centos_ccls()
     GCC_DIR=`which gcc`
     G_DIR=`which g++`
 
-    git clone https://git.llvm.org/git/llvm.git ${CURR_DIR}/llvm
-    git clone https://git.llvm.org/git/clang.git ${CURR_DIR}/llvm/tools/clang
+    git clone  --depth=1 https://git.llvm.org/git/llvm.git ${CURR_DIR}/llvm
+    git clone  --depth=1 https://git.llvm.org/git/clang.git ${CURR_DIR}/llvm/tools/clang
 
     cd ${CURR_DIR}/llvm
      
