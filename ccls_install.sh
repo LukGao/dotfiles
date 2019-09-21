80711a3800860889d8a5c414b9f37efcee9251ce
ggyyll <ggllyyx@gmail.com>
2019-09-21 15:31:39 +0800

    Update ubuntu ccls

--- ccls_install.sh
+++ ccls_install.sh
@@ -7,9 +7,9 @@ export OsName=""
 
 function ubuntu_ccls()
 {
-    CURR_DIR=`pwd`
     git clone --depth=1 --recursive https://github.com/MaskRay/ccls
     cd ccls 
+    CURR_DIR=`pwd`
     wget -c http://releases.llvm.org/8.0.0/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
     tar xf clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
     ${CMAKE} -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${CURR_DIR}/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04
