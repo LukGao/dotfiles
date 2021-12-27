d0b257548d7f85dcbe70ec3ebaa0181d36838ff5
gyl <im.ggyyll@gmail.com>
2021-12-27 19:56:28 +0800

    remove neomake

--- nvim/lua/plugin/plugin.lua
+++ nvim/lua/plugin/plugin.lua
@@ -4,9 +4,9 @@
 local plug_url_format = "https://github.com/%s"
 local packer_url = "https://github.com/wbthomason/packer.nvim"
 
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = vim.fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
+  packer_bootstrap = vim.fn.system({"git", "clone", "--depth", "1", packer_url, install_path})
 end
 
 
@@ -21,8 +21,8 @@ local packer_config =  {
 local plug_func = function()
     use{"nvim-lua/plenary.nvim"}
     use{"wbthomason/packer.nvim"}
-    use{"dracula/vim",                      as      = "dracula",config = function() vim.cmd[[silent! colorscheme dracula]] end}
-    use{"Yggdroot/LeaderF",                 run     = ":LeaderfInstallCExtension"}
+    use{"dracula/vim", as = "dracula",config = function() vim.cmd[[silent! colorscheme dracula]] end}
+    use{"Yggdroot/LeaderF", run = ":LeaderfInstallCExtension"}
     use{"nvim-treesitter/nvim-treesitter", config = require("plugin.treesitter")}
     use{"kyazdani42/nvim-web-devicons"}
     use{"nvim-lualine/lualine.nvim", config = require("plugin.lualine")}
@@ -41,28 +41,26 @@ local plug_func = function()
     use{"tpope/vim-surround"}
     use{"mg979/vim-visual-multi"}
     use{"scrooloose/nerdcommenter"}
-    use {'nvim-lua/popup.nvim'}
-    use {'nvim-treesitter/nvim-treesitter-refactor'}
-    use {'andymass/vim-matchup'}
-    use {'phaazon/hop.nvim', as = 'hop', config = function() require('hop').setup() end}
-    use {'folke/trouble.nvim', config = function() require('trouble').setup() end}
-    use {'xiyaowong/nvim-bqf', ft = 'qf',config = function() require('bqf').setup({
-    	vim.cmd[[
-    	augroup BQF
-			  au!
-			  au FileType qf nnoremap <silent> <esc> :cclose<CR>
-			augroup END
-    	]]
+    use{"nvim-lua/popup.nvim"}
+    use{"nvim-treesitter/nvim-treesitter-refactor"}
+    use{"andymass/vim-matchup"}
+    use{"phaazon/hop.nvim", as = "hop", config = function() require("hop").setup() end}
+    use{"folke/trouble.nvim", config = function() require("trouble").setup() end}
+    use{"xiyaowong/nvim-bqf", ft = "qf",config = function() require("bqf").setup({
+        vim.cmd[[
+        augroup BQF
+              au!
+              au FileType qf nnoremap <silent> <esc> :cclose<CR>
+            augroup END
+        ]]
 
     }) end}
-    use {'junegunn/fzf'}
-    use {"folke/twilight.nvim",config = function()require("twilight").setup()end}
-    use {"folke/zen-mode.nvim",config = function()  require("zen-mode").setup()end}
-	  use{"bagrat/vim-buffet"}
+    use{"junegunn/fzf"}
+    use{"folke/twilight.nvim",config = function() require("twilight").setup()end}
+    use{"bagrat/vim-buffet"}
     use{"skywind3000/vim-cppman"}
     use{"lfv89/vim-interestingwords"}
     use{"MattesGroeger/vim-bookmarks"}
-    use{"neomake/neomake"}
     use{"tpope/vim-abolish"}
     use{"skywind3000/asyncrun.vim"}
     use{"skywind3000/asynctasks.vim"}
@@ -85,7 +83,7 @@ local plug_func = function()
     use{"williamboman/nvim-lsp-installer", config = function() require("lsp").setup() end}
     use{"ray-x/lsp_signature.nvim", config = require("plugin.lsp_signature")}
     if packer_bootstrap then
-        require('packer').sync()
+        require("packer").sync()
     end
 end
 
