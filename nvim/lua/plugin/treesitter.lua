aaa7a55cf1a8077150fea969a725fcbf70448821
gyl <im.ggyyll@gmail.com>
2022-08-02 10:50:31 +0800

    enable treesitter fold

--- nvim/lua/plugin/treesitter.lua
+++ nvim/lua/plugin/treesitter.lua
@@ -11,6 +11,23 @@ return function()
             extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
             max_file_lines = nil, -- Do not enable for files with more than n lines, int
         },
+      -- 启用增量选择
+      --incremental_selection = {
+        --enable = true,
+        --keymaps = {
+          --init_selection = '<CR>',
+          --node_incremental = '<CR>',
+          --node_decremental = '<BS>',
+          --scope_incremental = '<TAB>',
+        --}
+      --},
+      -- 启用基于Treesitter的代码格式化(=) . NOTE: This is an experimental feature.
+      --indent = {
+        --enable = true
+      --}
     })
+    --vim.wo.foldlevel = 99
+    --vim.wo.foldmethod = 'expr'
+    --vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
     require("nvim-treesitter.install").prefer_git = true
 end
