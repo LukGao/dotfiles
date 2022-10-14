ddc5d6644f80cb009977dc03df15549ef8d4c5a8
gyl <im.ggyyll@gmail.com>
2022-10-14 23:15:31 +0800

    update install

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -2,82 +2,59 @@
 set -x
 export SUDO="sudo"
 export InstallCommand="default"
-export ToolsDir="$HOME/.config/dotfiles"
-export BinaryDir="$HOME/bin"
-export DotfilesDir=$PWD
 export DEBIAN_FRONTEND=noninteractive
-
-nvim_url=https://github.com/neovim/neovim/releases/download/v0.6.0/nvim-linux64.tar.gz
-fd_url=https://github.com/sharkdp/fd/releases/download/v8.2.1/fd-musl_8.2.1_amd64.deb
-rg_url=https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
-z_url=https://github.com/skywind3000/z.lua.git
-fzf_url=https://github.com/junegunn/fzf.git
+export BREW=/home/linuxbrew/.linuxbrew/bin/brew
 
 BasePath=$(cd `dirname $0`; pwd)
 
-echo "source $BasePath/bashrc" >> $HOME/.bashrc
-echo "export PATH=${BinaryDir}:\$PATH" >> $HOME/.bashrc
-
-[ ! -d ${BinaryDir} ] && mkdir -p ${BinaryDir}
-[ ! -d ${ToolsDir} ] && mkdir -p ${ToolsDir}
-
-mkdir -p $HOME/.tmux/plugins/fzf
-
-cp $BasePath/.inputrc $HOME
-cp $BasePath/.tmux.conf $HOME
-ln -sf $BasePath/nvim $HOME/.config/nvim
-cp $BasePath/tmux/fzf $HOME/.tmux/plugins/fzf/
-cp $BasePath/tmux/tm $HOME/.tmux/
-
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 if [[ $(id -u) -eq 0 ]];then
     export SUDO=""
-else
-    export SUDO="sudo"
 fi
 
-export PATH=${BinaryDir}:$PATH
+export InstallCommand="${SUDO} apt-get install -y "
+
+echo "source $BasePath/bashrc" >> ~/.bashrc
+
+rm -rf ~/.inputrc && cp .inputrc ~
+rm -rf ~/.tmux.conf && cp .tmux.conf ~
+rm -rf ~/.z.lua && mkdir ~/.z.lua && cp z.lua ~/.z.lua/z.lua
+rm -rf ~/.tmux/plugins/fzf && mkdir -p ~/.tmux/plugins/fzf && cp tmux/fzf ~/.tmux/plugins/fzf/ && cp tmux/tm ~/.tmux/
+rm -rf  ~/.local/share/nvim/site/pack/packer/start/packer.nvim && cp -r packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+ln -snf nvim ~/.config/nvim
 
 ${SUDO} apt-get update -qq
-
-export InstallCommand="${SUDO} apt-get install -y "
 
 ${InstallCommand} curl git tmux wget python3 python3-dev python3-pip lua5.1 > /dev/null  2>&1
 
 pip3 install -q neovim
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+install_homebrew()
+{
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
 
-cd $ToolsDir
-curl -sSfL "$nvim_url" | tar -xzf -
-ln -sf $PWD/nvim-linux64/bin/nvim ${BinaryDir}/nvim
-cd -
+install_homebrew.sh
 
-
-git clone --depth 1 ${z_url} ~/.z.lua
-
-git clone --depth 1 ${fzf_url} ~/.fzf && yes | ~/.fzf/install
-
-curl -sSfL -o rg.deb $rg_url && ${SUDO} dpkg -i rg.deb && rm -rf rg.deb
-
-curl -sSfL -o fd.deb $fd_url && ${SUDO} dpkg -i fd.deb && rm -rf fd.deb
-
-[[ -f .bashrc ]] && source .bashrc
-[[ -f .bash_profile ]] && source .bash_profile
-
-z -h
-fzf --version
-rg --version
-fd --version
-nvim --version
-tmux -V
+brew update
+# echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+unset HOMEBREW_BOTTLE_DOMAIN
+unset HOMEBREW_CORE_GIT_REMOTE
+unset HOMEBREW_BREW_GIT_REMOTE
+}
+command -v fzf > /dev/null || ($BREW install fzf && $($BREW --prefix)/opt/fzf/install)
+command -v fd > /dev/null || $BREW install fd
+command -v rg > /dev/null || $BREW install rg
+command -v node > /dev/null || $BREW install node
+command -v tmux > /dev/null || $BREW install tmux
 
 git config --global alias.tree "log --graph --all --relative-date --abbrev-commit --format=\"%x09 %h %Cgreen%cd%Creset [%Cblue%cn%Creset] %C(auto)%d%Creset %s\""
 git config --global commit.gpgsign true
-git config --global core.editor "vim"
+git config --global core.editor "nvim"
 
-# set_proxy()
-# {
 # tee ~/.curlrc <<-'EOF'
 # socks5=127.0.0.1:1080
 # EOF
@@ -86,4 +63,3 @@ git config --global core.editor "vim"
 # [http]
 #    proxy = socks5://127.0.0.1:1080
 # EOF
-# }
