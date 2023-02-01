df1ccc359ff9c1f64876f45da88e1aac70339e14
gyl <im.ggyyll@gmail.com>
2023-02-01 23:07:02 +0800

    add disabled_plugins

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -66,27 +66,41 @@ vim.opt.viewdir = cache_dir .. 'view/'
 vim.opt.spellfile = cache_dir .. 'spell/en.uft-8.add'
 vim.opt.clipboard = 'unnamedplus'
 
-vim.g.loaded_gzip = 1
-vim.g.loaded_zip = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_tar = 1
-vim.g.loaded_tarPlugin = 1
+local disabled_plugins = {
+	'2html_plugin',
+	'getscript',
+	'getscriptPlugin',
+	'gzip',
+	'logipat',
+	'netrw',
+	'netrwPlugin',
+	'netrwSettings',
+	'netrwFileHandlers',
+	'matchit',
+	'tar',
+	'tarPlugin',
+	'rrhelper',
+	'spellfile_plugin',
+	'vimball',
+	'vimballPlugin',
+	'zip',
+	'zipPlugin',
+	'node_provider',
+	'ruby_provider',
+	'perl_provider',
+	'tutor',
+	'rplugin',
+	'syntax',
+	'synmenu',
+	'optwin',
+	'compiler',
+	'bugreport',
+	'ftplugin',
+}
 
-vim.g.loaded_getscript = 1
-vim.g.loaded_getscriptPlugin = 1
-vim.g.loaded_vimball = 1
-vim.g.loaded_vimballPlugin = 1
-vim.g.loaded_2html_plugin = 1
-
-vim.g.loaded_matchit = 1
-vim.g.loaded_matchparen = 1
-vim.g.loaded_logiPat = 1
-vim.g.loaded_rrhelper = 1
-
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
-vim.g.loaded_netrwFileHandlers = 1
+for _, plugin in pairs(disabled_plugins) do
+	vim.g['loaded_' .. plugin] = 1
+end
 
 vim.g.python_host_prog = '/user/bin/python'
 vim.g.python3_host_prog = '/usr/bin/python3'
