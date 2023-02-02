ff21f9201aecf5f777e8fe26d15b85b1b6161f74
gyl <im.ggyyll@gmail.com>
2023-02-02 22:45:21 +0800

    add bufferline

--- nvim/lua/plugin/plugin.lua
+++ nvim/lua/plugin/plugin.lua
@@ -59,33 +59,166 @@ local interestingwords_cfg = function()
 end
 
 local buffet_cfg = function()
-    vim.cmd [[
-    nmap <leader>1 <Plug>BuffetSwitch(1)
-    nmap <leader>2 <Plug>BuffetSwitch(2)
-    nmap <leader>3 <Plug>BuffetSwitch(3)
-    nmap <leader>4 <Plug>BuffetSwitch(4)
-    nmap <leader>5 <Plug>BuffetSwitch(5)
-    nmap <leader>6 <Plug>BuffetSwitch(6)
-    nmap <leader>7 <Plug>BuffetSwitch(7)
-    nmap <leader>8 <Plug>BuffetSwitch(8)
-    nmap <leader>9 <Plug>BuffetSwitch(9)
-    nmap <leader>0 <Plug>BuffetSwitch(10)
+    vim.opt.termguicolors = true
+    local colors = {
+        white = "#abb2bf",
+        darker_black = "#1b1f27",
+        black = "#1e222a", --  nvim bg
+        black2 = "#252931",
+        one_bg = "#282c34", -- real bg of onedark
+        one_bg2 = "#353b45",
+        one_bg3 = "#30343c",
+        grey = "#42464e",
+        grey_fg = "#565c64",
+        grey_fg2 = "#6f737b",
+        light_grey = "#6f737b",
+        red = "#d47d85",
+        baby_pink = "#DE8C92",
+        pink = "#ff75a0",
+        line = "#2a2e36", -- for lines like vertsplit
+        green = "#A3BE8C",
+        vibrant_green = "#7eca9c",
+        nord_blue = "#81A1C1",
+        blue = "#61afef",
+        yellow = "#e7c787",
+        sun = "#EBCB8B",
+        purple = "#b4bbc8",
+        dark_purple = "#c882e7",
+        teal = "#519ABA",
+        orange = "#fca2aa",
+        cyan = "#a3b8ef",
+        statusline_bg = "#22262e",
+        lightbg = "#2d3139",
+        lightbg2 = "#262a32",
+        pmenu_bg = "#A3BE8C",
+        folder_bg = "#61afef",
+    }
 
-    let g:buffet_tab_icon = "\uf00a"
-    let g:buffet_tab_icon = "\uf00a"
-    let g:buffet_show_index = 1
-    let g:buffet_powerline_separators = 1
-    let g:buffet_left_trunc_icon = "\uf0a8"
-    let g:buffet_right_trunc_icon = "\uf0a9"
+    require('bufferline').setup {
+        options = {
+            numbers = "none",
+            close_command = "bdelete! %d",
+            right_mouse_command = "vertical sbuffer %d",
+            left_mouse_command = function(buf_id)
+                local win_ids = vim.fn.win_findbuf(buf_id)
+                if win_ids[1] then
+                    vim.fn.win_gotoid(win_ids[1])
+                else
+                    vim.cmd(string.format("tabe %s", vim.api.nvim_buf_get_name(buf_id)))
+                    -- vim.cmd(string.format("vertical sbuffer %d", buf_id))
+                end
+            end,
+            middle_mouse_command = nil,
+            indicator = {
+                icon = '▎',
+                style = 'icon',
+            },
+            buffer_close_icon = ' ',
+            modified_icon = ' ',
+            close_icon = '',
+            left_trunc_marker = '',
+            right_trunc_marker = '',
+            -- display buffer group by tabpage
+            -- custom_filter = function(buf_id)
+            --   return vim.tbl_contains(vim.fn.tabpagebuflist(), buf_id)
+            -- end,
+            max_name_length = 20,
+            max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+            tab_size = 15,
+            diagnostics = false,
+            offsets = { { filetype = 'NvimTree', text = 'File Explorer', highlight = 'Directory' } },
+            show_buffer_icons = true,
+            show_buffer_close_icons = true,
+            show_close_icon = true,
+            show_tab_indicators = true,
+            persist_buffer_sort = true,
+            separator_style = 'thin',
+            enforce_regular_tabs = false,
+            always_show_bufferline = false,
+            sort_by = "tabs",
+        },
+        highlights = {
+            background = {
+                fg = colors.grey_fg,
+                bg = colors.black2,
+            },
 
-    function! g:BuffetSetCustomColors()
-        hi! BuffetCurrentBuffer cterm=NONE ctermbg=106 ctermfg=8 guibg=#b8bb26 guifg=#000000
-        hi! BuffetTrunc cterm=bold ctermbg=66 ctermfg=8 guibg=#458588 guifg=#000000
-        hi! BuffetBuffer cterm=NONE ctermbg=239 ctermfg=8 guibg=#504945 guifg=#000000
-        hi! BuffetTab cterm=NONE ctermbg=66 ctermfg=8 guibg=#458588 guifg=#000000
-        hi! BuffetActiveBuffer cterm=NONE ctermbg=10 ctermfg=239 guibg=#999999 guifg=#504945
-    endfunction
-    ]]
+            -- buffers
+            buffer_selected = {
+                fg = colors.white,
+                bg = colors.black,
+                bold = true,
+                italic = false,
+            },
+            buffer_visible = {
+                fg = colors.light_grey,
+                bg = colors.black2,
+            },
+
+            -- close buttons
+            close_button = {
+                fg = colors.light_grey,
+                bg = colors.black2,
+            },
+            close_button_visible = {
+                fg = colors.light_grey,
+                bg = colors.black2,
+            },
+            close_button_selected = {
+                fg = colors.light_grey,
+                bg = colors.black,
+            },
+            fill = {
+                fg = colors.grey_fg,
+                bg = colors.black2,
+            },
+            indicator_selected = {
+                fg = colors.blue,
+                bg = colors.black,
+            },
+
+            -- modified
+            modified = {
+                fg = colors.red,
+                bg = colors.black2,
+            },
+            modified_visible = {
+                fg = colors.red,
+                bg = colors.black2,
+            },
+            modified_selected = {
+                fg = colors.green,
+                bg = colors.black,
+            },
+
+            -- separators
+            separator = {
+                fg = colors.black2,
+                bg = colors.black2,
+            },
+            separator_visible = {
+                fg = colors.black2,
+                bg = colors.black2,
+            },
+            separator_selected = {
+                fg = colors.black2,
+                bg = colors.black2,
+            },
+            -- tabs
+            tab = {
+                fg = colors.light_grey,
+                bg = colors.one_bg3,
+            },
+            tab_selected = {
+                fg = colors.black2,
+                bg = colors.nord_blue,
+            },
+            tab_close = {
+                fg = colors.light_grey,
+                bg = colors.black,
+            },
+        }
+    }
 
 end
 
@@ -213,7 +346,7 @@ local plugins = {
     { "mhinz/vim-startify", config = startify_cfg },
 
     { "SmiteshP/nvim-gps", config = gps_cfg, event = 'VeryLazy', },
-    { "kyazdani42/nvim-web-devicons", event = 'VeryLazy', },
+    { "nvim-tree/nvim-web-devicons", event = 'VeryLazy', },
     { "tpope/vim-endwise", event = 'VeryLazy', },
     { "scrooloose/nerdcommenter", event = 'VeryLazy', },
     { "vim-scripts/DoxygenToolkit.vim", event = 'VeryLazy', },
@@ -235,7 +368,51 @@ local plugins = {
     { "sunjon/shade.nvim", config = shade_cfg, event = 'VeryLazy', },
     { "neoclide/coc.nvim", branch = "release", event = 'VeryLazy', },
     { "preservim/tagbar", config = tagbar_cfg, event = 'VeryLazy', },
-    { "bagrat/vim-buffet", config = buffet_cfg, event = 'VeryLazy', },
+    {
+        "akinsho/nvim-bufferline.lua",
+        event = "VeryLazy",
+        dependencies = { { "nvim-tree/nvim-web-devicons" }, { "catppuccin/nvim" } },
+
+        init = function()
+            for i = 1, 9 do
+                vim.keymap.set("n", "<leader>" .. i, function()
+                    require("bufferline").go_to_buffer(i, true)
+                end)
+            end
+
+            vim.keymap.set("n", "<leader>" .. 0, function()
+                require("bufferline").go_to_buffer(-1, true)
+            end)
+
+            vim.keymap.set("n", "<Tab>", function()
+                require("bufferline").cycle(1)
+            end)
+            vim.keymap.set("n", "<S-Tab>", function()
+                require("bufferline").cycle(-1)
+            end)
+        end,
+
+        config = function()
+            require("bufferline").setup({
+                options = {
+                    mode = "buffers",
+                    numbers = "ordinal",
+                    highlights = require("catppuccin.groups.integrations.bufferline").get(),
+                    indicator = { icon = "" },
+                    max_name_length = 10,
+                    max_prefix_length = 2,
+                    modified_icon = "●",
+                    persist_buffer_sort = false,
+                    show_buffer_close_icons = false,
+                    show_buffer_icons = false,
+                    show_close_icon = false,
+                    name_formatter = function(opts)
+                        return string.format("%s", opts.name)
+                    end,
+                },
+            })
+        end,
+    },
     { "luochen1990/rainbow", config = rainbow_cfg, event = 'VeryLazy', },
     { "skywind3000/asynctasks.vim", config = asynctasks_cfg, event = 'VeryLazy', },
     { "lfv89/vim-interestingwords", config = interestingwords_cfg, event = 'VeryLazy', },
