7aee908741f54f5bc6af99d7f7981fe01b2f3a44
gyl30 <im.ggyyll@gmail.com>
2023-05-07 19:43:07 +0800

    update python host prog

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,7 +9,7 @@ vim.opt.termguicolors=true
 vim.opt.number=true
 vim.opt.showcmd=false
 vim.opt.mouse=''
--- vim.opt.cursorline=true
+vim.opt.cursorline=true
 vim.opt.ttimeoutlen=0
 vim.opt.laststatus=2
 vim.opt.cmdheight=1
@@ -44,6 +44,8 @@ vim.opt.foldcolumn = '1'
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
+vim.opt.list = true
+vim.opt.listchars = 'tab:»·,nbsp:+,trail:·,extends:→,precedes:←'
 vim.cmd[[
 autocmd FileType * setlocal formatoptions-=o
 ]]
@@ -66,54 +68,27 @@ vim.opt.viewdir = cache_dir .. 'view/'
 vim.opt.spellfile = cache_dir .. 'spell/en.uft-8.add'
 vim.opt.clipboard = 'unnamedplus'
 
-local disabled_plugins = {
-	'2html_plugin',
-	'getscript',
-	'getscriptPlugin',
-	'gzip',
-	'logipat',
-	'netrw',
-	'netrwPlugin',
-	'netrwSettings',
-	'netrwFileHandlers',
-	'matchit',
-	'tar',
-	'tarPlugin',
-	'rrhelper',
-	'spellfile_plugin',
-	'vimball',
-	'vimballPlugin',
-	'zip',
-	'zipPlugin',
-	'node_provider',
-	'ruby_provider',
-	'perl_provider',
-	'tutor',
-	'rplugin',
-	'syntax',
-	'synmenu',
-	'optwin',
-	'compiler',
-	'bugreport',
-	'ftplugin',
-}
-
-for _, plugin in pairs(disabled_plugins) do
-	vim.g['loaded_' .. plugin] = 1
-end
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
 
-vim.g.python_host_prog = '/user/bin/python'
-vim.g.python3_host_prog = '/usr/bin/python3'
 
-if vim.fn.empty(vim.fn.glob('/opt/rh/rh-python38/root/bin/python')) == 0 then
-    vim.g.python3_host_prog = '/opt/rh/rh-python38/root/bin/python'
-    vim.g.python_host_prog = '/opt/rh/rh-python38/root/bin/python'
-end
+ if vim.fn.executable('rg') == 1 then
+     vim.opt.grepformat = '%f:%l:%c:%m,%f:%l:%m'
+     vim.opt.grepprg = 'rg --vimgrep --no-heading --smart-case'
+ end
 
-if vim.loop.os_uname().sysname == 'Darwin' then
-    vim.g.python3_host_prog = '/usr/local/bin/python3'
-end
+ local python2_dirs={'/user/bin/python','/usr/local/bin/python','/home/linuxbrew/.linuxbrew/bin/python'}
+ local python3_dirs={'/user/bin/python3','/usr/local/bin/python3','/home/linuxbrew/.linuxbrew/bin/python3'}
 
+ for index, dir in pairs(python2_dirs) do
+     if vim.loop.fs_stat(dir) then
+         vim.g.python_host_prog = dir
+     end
+ end
+
+ for index, dir in pairs(python3_dirs) do
+     if vim.loop.fs_stat(dir) then
+         vim.g.python3_host_prog = dir
+     end
+ end
