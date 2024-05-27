8854fbf846ee1a0b7eb34bb429698a1b46721e1a
gyl30 <im.ggyyll@gmail.com>
2024-05-27 11:18:26 +0800

    update

--- bashrc
+++ bashrc
@@ -35,20 +35,44 @@ function _fish_collapsed_pwd() {
     local IFS="/"
     echo "${elements[*]}"
 }
-orange=$(tput setaf 1);
-yellow=$(tput setaf 4);
-green=$(tput setaf 12); 
-bold=$(tput bold); 
-reset=$(tput sgr0);
+
+function brew_disable() {
+        export PATH=${PATH##*"/.linuxbrew/bin:"}
+        export PATH=${PATH##*"/.linuxbrew/sbin:"}
+        export MANPATH=${MANPATH##*"/.linuxbrew/share/man:"}
+        export INFOPATH=${INFOPATH##*"/.linuxbrew/share/info:"}
+}
+
+function brew_enable() {
+        BREW='/home/linuxbrew/.linuxbrew'
+        brew_disable
+        export PATH="$BREW/bin:$BREW/sbin:$PATH"
+        export MANPATH="$BREW/share/man:$MANPATH"
+        export INFOPATH="$BREW/share/info:$INFOPATH"
+        export HOMEBREW_NO_AUTO_UPDATE=1
+}
+
+# homebrew
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
+export HOMEBREW_API_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/api"
+export HOMEBREW_NO_AUTO_UPDATE=1
+
+# bash PS1
 if [ -n "$BASH_VERSION" ]; then
     if [ "$UID" -eq 0 ]; then
-        #export PS1='\[\e[38;5;135m\]\u\[\e[0m\]@\[\e[38;5;166m\]\h\[\e[0m\] \[\e[31m\]$(_fish_collapsed_pwd)\[\e[0m\] \[\e[31m\]➜\[\e[0m\] '
         export PS1='\[\e[38;5;135m\]•\[\e[0m\] •\[\e[38;5;166m\] •\[\e[0m\] \[\e[32m\]$(_fish_collapsed_pwd)\[\e[0m\] \[\e[31m\]➜\[\e[0m\] '
     else
-        #export PS1="\[${bold}\] \[${orange}\]>\[${yellow}\]>\[${reset}\] $(_fish_collapsed_pwd) ➜ "
         export PS1='\[\e[38;5;135m\]•\[\e[0m\] •\[\e[38;5;166m\] •\[\e[0m\] \[\e[32m\]$(_fish_collapsed_pwd)\[\e[0m\] \[\e[32m\]➜\[\e[0m\] '
     fi
 fi
+
+# ssh
+[ -z $SSH_TTY ] || PS1='\[\e[0;30m\]\[\e[47m\]SSH\[\e[0m\] '$PS1
+
+
+# fzf
 export RUNEWIDTH_EASTASIAN=0
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export BAT_THEME="Dracula"
@@ -56,6 +80,7 @@ export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_mo
 export FZF_DEFAULT_OPTS="--no-info --no-scrollbar --height 60% --layout=reverse --ansi --border=none"
 export FZF_CTRL_T_OPTS="--no-info --no-scrollbar --height 60% --layout=reverse --ansi --border=none --preview 'bat -n --color=always --line-range=:500 {}'"
 
+# alias
 alias zc='z -c'      # 严格匹配当前路径的子路径
 alias zf='z -I'      # 使用 fzf 对多个结果进行选择
 alias zb='z -b'      # 快速回到父目录
@@ -64,19 +89,26 @@ alias ok='nvim'
 alias tmux='tmux -2'
 
 alias vi='nvim -u NONE -N'
+alias vim="nvim -u NONE"
 
 alias proxy='export all_proxy=socks5://127.0.0.1:1080'
 alias unproxy='unset all_proxy'
 
-export PATH="$HOME/go/bin:$PATH"
-export GO111MODULE=on
-export GOPROXY=https://goproxy.cn,direct
+# golang
+if command -v go &> /dev/null
+    export PATH="$HOME/go/bin:$PATH"
+    export GO111MODULE=on
+    export GOPROXY=https://goproxy.cn,direct
+fi
 
-[ -z $SSH_TTY ] || PS1='\[\e[0;30m\]\[\e[47m\]SSH\[\e[0m\] '$PS1
-
+# bash history
 HISTSIZE=99999
 HISTFILESIZE=99999
 shopt -s histappend
-PROMPT_COMMAND='history -a' # append history file after each command
+PROMPT_COMMAND='history -a'
+
+# git
 alias git-ls="\\ls -A --group-directories-first -1 | while IFS= read -r line; do git log --color --format=\"\$(\\ls -d -F --color \"\$line\") =} %C(bold black)▏%Creset%Cred%h %Cgreen(%cr)%Creset =} %C(bold black)▏%Creset%s %C(bold blue)<%an>%Creset\" --abbrev-commit --max-count 1 HEAD -- \"\$line\"; done | awk -F'=}' '{ nf[NR]=NF; for (i = 1; i <= NF; i++) { cell[NR,i] = \$i; gsub(/\\033\\[([[:digit:]]+(;[[:digit:]]+)*)?[mK]/, \"\", \$i); len[NR,i] = l = length(\$i); if (l > max[i]) max[i] = l; } } END { for (row = 1; row <= NR; row++) { for (col = 1; col < nf[row]; col++) printf \"%s%*s%s\", cell[row,col], max[col]-len[row,col], \"\", OFS; print cell[row,nf[row]]; } }'"
+
+# z.lua
 eval "$(lua ~/.z.lua/z.lua --init bash once enhanced echo fzf)"
--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -2,11 +2,9 @@
 set -x
 export SUDO="sudo"
 export NONINTERACTIVE=1
-export InstallCommand="default"
 export DEBIAN_FRONTEND=noninteractive
-export BREW=/home/linuxbrew/.linuxbrew/bin/brew
 
-BasePath=$(cd `dirname $0`; pwd)
+PROJECT_PATH=$(cd `dirname $0`; pwd)
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
@@ -16,59 +14,53 @@ fi
 
 install_dep()
 {
-    export InstallCommand="${SUDO} apt-get install -y "
-
-    echo "source $BasePath/bashrc" >> ~/.bashrc
+    echo "source $PROJECT_PATH/bashrc" >> ~/.bashrc
 
     ${SUDO} apt-get update -qq
 
-    ${InstallCommand} build-essential curl git tmux wget python3 python3-dev python3-pip lua5.1 > /dev/null  2>&1
+    {SUDO} apt-get install -y build-essential curl git tmux wget python3 python3-dev python3-pip lua5.1 > /dev/null  2>&1
 }
 
 copy_dotfile()
 {
-
-    rm -rf ~/.inputrc && cp .inputrc ~
-    rm -rf ~/.tmux.conf && cp .tmux.conf ~
-    rm -rf ~/.z.lua && mkdir ~/.z.lua && cp z.lua ~/.z.lua/z.lua
-    rm -rf ~/.tmux/plugins/fzf && mkdir -p ~/.tmux/plugins/fzf && cp tmux/fzf ~/.tmux/plugins/fzf/ && cp tmux/tm ~/.tmux/
-    git clone https://ghproxy.com/https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    rm -rf ~/.inputrc 
+    cp .inputrc ~
+    rm -rf ~/.tmux.conf 
+    cp .tmux.conf ~
+    rm -rf ~/.z.lua 
+    mkdir ~/.z.lua 
+    cp z.lua ~/.z.lua/z.lua
+    rm -rf ~/.tmux/plugins/fzf 
+    mkdir -p ~/.tmux/plugins/fzf 
+    cp tmux/fzf ~/.tmux/plugins/fzf/ 
+    cp tmux/tm ~/.tmux/
     mkdir ~/.config
-    ln -snf ${BasePath}/nvim ~/.config/nvim
+    ln -snf ${PROJECT_PATH}/nvim ~/.config/nvim
 }
-
-
 
 install_homebrew()
 {
+    export HOMEBREW_NO_AUTO_UPDATE=1
     export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
     export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
+    export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
+    export HOMEBREW_API_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/api"
+    /bin/bash -c "$(curl -fsSL https://mirrors.ustc.edu.cn/misc/brew-install.sh)"
+    sed -i '/check-run-command-as-root$/d' /home/linuxbrew/.linuxbrew/Homebrew/Library/Homebrew/brew.sh
+    /home/linuxbrew/.linuxbrew/bin/brew update --force --quiet
+    /home/linuxbrew/.linuxbrew/bin/brew update --force --quiet
 
-    set +x
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://gitee.com/ineo6/homebrew-install/raw/master/install.sh)"
-    set -x
-
-    git -C "$($BREW --repo)" remote set-url origin https://mirrors.ustc.edu.cn/brew.git
-    git -C "$($BREW --repo homebrew/core)" remote set-url origin https://mirrors.ustc.edu.cn/linuxbrew-core.git
-    git -C "$($BREW --repo homebrew/cask)" remote set-url origin https://mirrors.ustc.edu.cn/homebrew-cask.git
-
-    $BREW update
-
-    command -v fzf > /dev/null || ($BREW install fzf && $($BREW --prefix)/opt/fzf/install --all)
-    command -v fd > /dev/null || $BREW install fd
-    command -v rg > /dev/null || $BREW install rg
-    command -v bat > /dev/null || $BREW install bat
-    command -v node > /dev/null || $BREW install node
-    command -v tmux > /dev/null || $BREW install tmux
-    command -v nvim > /dev/null || $BREW install neovim
-    $BREW  install --HEAD universal-ctags/universal-ctags/universal-ctags
-    unset HOMEBREW_CORE_GIT_REMOTE
-    unset HOMEBREW_BREW_GIT_REMOTE
-
-    echo "eval \"\$($($BREW --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+    /home/linuxbrew/.linuxbrew/bin/brew install fzf && $(/home/linuxbrew/.linuxbrew/bin/brew --prefix)/opt/fzf/install --all
+    /home/linuxbrew/.linuxbrew/bin/brew install fd
+    /home/linuxbrew/.linuxbrew/bin/brew install rg
+    /home/linuxbrew/.linuxbrew/bin/brew install bat
+    /home/linuxbrew/.linuxbrew/bin/brew install tmux
+    /home/linuxbrew/.linuxbrew/bin/brew install neovim
+    /home/linuxbrew/.linuxbrew/bin/brew  install --HEAD universal-ctags/universal-ctags/universal-ctags
 }
 git_config()
 {
+    git config --global http.https://github.com.proxy socks5h://127.0.0.1:1080
     git config --global alias.tree "log --graph --all --relative-date --abbrev-commit --format=\"%x09 %h %Cgreen%cd%Creset [%Cblue%cn%Creset] %C(auto)%d%Creset %s\""
     git config --global commit.gpgsign true
     git config --global core.editor "nvim"
@@ -84,9 +76,4 @@ git_config
 
 # tee ~/.curlrc <<-'EOF'
 # socks5=127.0.0.1:1080
-# EOF
-
-# tee ~/.gitconfig <<-'EOF'
-# [http]
-#    proxy = socks5://127.0.0.1:1080
 # EOF
